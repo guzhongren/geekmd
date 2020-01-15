6d8f1757588036d035271b9fc942ac55277211ee
guzhongren <guzhongren@live.cn>
2020-01-15 16:33:07 +0800

    fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'import/extensions': ['error', 'always', {
       js: 'never',
       mjs: 'never',
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
 import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
+      // eslint-disable-next-line no-console
       console.log('App is being served from cache by a service worker.\n'
                 + 'For more details, visit https://goo.gl/AFskqB');
     },
